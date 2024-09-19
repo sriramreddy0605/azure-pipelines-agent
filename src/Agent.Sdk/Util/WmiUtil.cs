cf37f0055b776af19647dddb8f0a0d7100bcd6bc
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2024-09-19 19:44:29 +0400

    Add wmi util

---
+++ src/Agent.Sdk/Util/WmiUtil.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Management;
+using System.Runtime.Versioning;
+
+namespace Agent.Sdk.Util;
+
+[SupportedOSPlatform("windows")]
+public class WmiUtil
+{
+    public static Task<List<ManagementBaseObject>> QueryGet(string query, CancellationToken cancellationToken)
+    {
+        var output = new List<ManagementBaseObject>();
+        var completionSource = new TaskCompletionSource<List<ManagementBaseObject>>();
+
+        var observer = new ManagementOperationObserver();
+        observer.ObjectReady += (sender, obj) =>
+        {
+            output.Add(obj.NewObject);
+        };
+        observer.Completed += (sender, e) =>
+        {
+            switch (e.Status)
+            {
+                case ManagementStatus.CallCanceled:
+                    completionSource.SetCanceled(cancellationToken);
+                    break;
+
+                case ManagementStatus.NoError:
+                    completionSource.SetResult(output);
+                    break;
+
+                default:
+                    completionSource.SetException(new Exception($"WMI Get Query failed with status {e.Status}"));
+                    break;
+            }
+        };
+
+        cancellationToken.Register(() =>
+        {
+            observer.Cancel();
+        });
+
+        using var searcher = new ManagementObjectSearcher(query);
+        searcher.Get(observer);
+
+        return completionSource.Task;
+    }
+}
