1edb4ec7476d1631cdd974b0a5811bf017b50d34
Mantavya Dhingra <145761344+MantavyaDh@users.noreply.github.com>
2025-01-17 14:06:10 +0530

    FIX: [CodeQL: SM02184] Server certificate validation disabled in VssUtil.cs (#5068)
    * Changing the callback checking for custom certificate validation
    
    * Adding Unit Test and Comments

--- src/Agent.Sdk/Util/VssUtil.cs
+++ src/Agent.Sdk/Util/VssUtil.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Net.Http;
+using System.Net.Security;
 using Microsoft.TeamFoundation.DistributedTask.WebApi;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
@@ -163,21 +164,23 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
             return true;
         }
 
+        // The function is to check if the custom server certificate validation is supported on the current platform.
         private static bool CheckSupportOfCustomServerCertificateValidation(ITraceWriter trace)
         {
             using (var handler = new HttpClientHandler())
             {
-                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return errors == SslPolicyErrors.None; };
 
                 using (var client = new HttpClient(handler))
                 {
                     try
                     {
                         client.GetAsync(_testUri).GetAwaiter().GetResult();
+                        trace.Verbose("Custom Server Validation Callback Successful, SSL diagnostic data collection is enabled.");
                     }
                     catch (Exception e)
                     {
-                        trace.Verbose($"SSL diagnostic data collection is disabled, due to issue:\n{e.Message}");
+                        trace.Verbose($"Custom Server Validation Callback Unsuccessful, SSL diagnostic data collection is disabled, due to issue:\n{e.Message}");
                         return false;
                     }
                     return true;
