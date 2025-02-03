4efa5b2eb6686224ea068ffc18a0427c88442313
sanjuyadav24 <yadavsanju800+microsoft@gmail.com>
2025-02-03 10:21:36 +0530

    Added WIF support for checkout task (#5085)
    * Added WIF support for checkout task
    
    * Added Test cases
    
    * Code review changes

--- src/Test/L0/Plugin/TestGitSourceProvider/MockAgentTaskPluginExecutionContext.cs
+++ src/Test/L0/Plugin/TestGitSourceProvider/MockAgentTaskPluginExecutionContext.cs
@@ -1,9 +1,14 @@
-﻿using Agent.Sdk;
+﻿using Agent.Plugins.Repository;
+using Agent.Sdk;
+using Moq;
+using System.Collections.Generic;
 
 namespace Test.L0.Plugin.TestGitSourceProvider
 {
     public class MockAgentTaskPluginExecutionContext : AgentTaskPluginExecutionContext
     {
         public MockAgentTaskPluginExecutionContext(ITraceWriter trace) : base(trace) { }
+
+        public override void PrependPath(string directory) { }
     }
 }
---
+++ src/Test/L0/Plugin/TestGitSourceProvider/MockGitSoureProvider.cs
@@ -0,0 +1,43 @@
+﻿using Agent.Plugins.Repository;
+using System.Collections.Generic;
+using System.Threading;
+using Agent.Sdk;
+using Microsoft.TeamFoundation.DistributedTask.Pipelines;
+using Microsoft.TeamFoundation.DistributedTask.WebApi;
+
+namespace Test.L0.Plugin.TestGitSourceProvider
+{
+    public class MockGitSoureProvider : GitSourceProvider
+    {
+        protected override GitCliManager GetCliManager(Dictionary<string, string> gitEnv = null)
+        {
+            return new MockGitCliManager();
+        }
+
+        protected override string GetWISCToken(ServiceEndpoint endpoint, AgentTaskPluginExecutionContext executionContext, CancellationToken cancellationToken)
+        {
+            return "WSICToken";
+        }
+        public override bool GitLfsSupportUseAuthHeader(AgentTaskPluginExecutionContext executionContext, GitCliManager gitCommandManager)
+        {
+            return false;
+        }
+
+        public override bool GitSupportsConfigEnv(AgentTaskPluginExecutionContext executionContext, GitCliManager gitCommandManager)
+        {
+            return false;
+        }
+
+        public override bool GitSupportsFetchingCommitBySha1Hash(GitCliManager gitCommandManager)
+        {
+            return false;
+        }
+
+        public override bool GitSupportUseAuthHeader(AgentTaskPluginExecutionContext executionContext, GitCliManager gitCommandManager)
+        {
+            return false;
+        }
+
+        public override void RequirementCheck(AgentTaskPluginExecutionContext executionContext, RepositoryResource repository, GitCliManager gitCommandManager){ }
+    }
+}
