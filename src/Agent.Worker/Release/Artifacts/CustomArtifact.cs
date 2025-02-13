eda24ce5a728c1a24a2820d20c820d1e559b54b4
Brian <b-barthel@github.com>
2025-02-13 09:43:43 -0500

    Update ADO Client package to 251 (#5112)
    * update to m251 version of ADO client package
    
    * Update dependencies and react to interface changes
    
    * Switch to BouncyCastle RSA Provider.
    
    ---------
    
    Co-authored-by: Brian Barthel <brbarthe@microsoft.com>

--- src/Agent.Worker/Release/Artifacts/CustomArtifact.cs
+++ src/Agent.Worker/Release/Artifacts/CustomArtifact.cs
@@ -40,7 +40,8 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Release.Artifacts
                     customArtifactDetails.ResultSelector,
                     customArtifactDetails.ResultTemplate,
                     customArtifactDetails.AuthorizationHeaders?.Select(header => ToAuthorizationHeader(header)).ToList(),
-                    customArtifactDetails.ArtifactVariables);
+                    customArtifactDetails.ArtifactVariables,
+                    new BouncyCastleRsaProvider());  
 
                 var artifactDownloadDetailList = new List<CustomArtifactDownloadDetails>();
                 artifactDetails.ToList().ForEach(x => artifactDownloadDetailList.Add(JToken.Parse(x).ToObject<CustomArtifactDownloadDetails>()));
@@ -123,7 +124,8 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Release.Artifacts
                 {
                     IEndpointAuthorizer authorizer = SchemeBasedAuthorizerFactory.GetEndpointAuthorizer(
                         ToServiceEndpoint(customArtifactDetails.Endpoint),
-                        customArtifactDetails.AuthorizationHeaders?.Select(header => ToAuthorizationHeader(header)).ToList());
+                        customArtifactDetails.AuthorizationHeaders?.Select(header => ToAuthorizationHeader(header)).ToList(),
+                        new BouncyCastleRsaProvider()); 
 
                     using (HttpWebResponse webResponse = GetWebResponse(executionContext, artifact.DownloadUrl, authorizer))
                     {
@@ -198,7 +200,8 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Release.Artifacts
                     customArtifactDetails.VersionsResultSelector,
                     customArtifactDetails.VersionsResultTemplate,
                     customArtifactDetails.AuthorizationHeaders?.Select(header => ToAuthorizationHeader(header)).ToList(),
-                    customArtifactDetails.ArtifactVariables);
+                    customArtifactDetails.ArtifactVariables,
+                    new BouncyCastleRsaProvider());  
 
                 foreach (var version in versions)
                 {
