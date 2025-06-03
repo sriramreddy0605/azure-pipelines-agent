9c887e98a5cf4efeb8e3273147e1f97d04b46605
dassayantan24 <dassayantan@microsoft.com>
2025-06-03 11:03:45 +0530

    [Bug] An error occurred trying to start process | Revert changes 10024e8 (#5229)
    * Revert changes 10024e8
    
    * Revert only try dispose the streams

--- src/Agent.Sdk/ProcessInvoker.cs
+++ src/Agent.Sdk/ProcessInvoker.cs
@@ -404,10 +404,6 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
             {
                 if (_proc != null)
                 {
-                    //Dispose the standard output/error stream. Refer: https://github.com/dotnet/runtime/issues/58872
-                    _proc.StandardOutput?.Dispose();
-                    _proc.StandardError?.Dispose();
-
                     _proc.Dispose();
                     _proc = null;
                 }
