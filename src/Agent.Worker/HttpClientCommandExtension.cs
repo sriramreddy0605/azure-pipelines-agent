8531d3c19eb371a6d93519504adf0ed3a512f4ba
Tarun Ramsinghani <tr@tr.com>
2025-06-13 15:34:08 +0530

    ADding test extension to test for proxy

---
+++ src/Agent.Worker/HttpClientCommandExtension.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Microsoft.VisualStudio.Services.Agent.Worker
+{
+    public sealed class AgentHealthProbeCommandExtension : BaseWorkerCommandExtension
+    {
+        public AgentHealthProbeCommandExtension()
+        {
+            CommandArea = "agenthealthprobe";
+            SupportedHostTypes = HostTypes.All;
+            InstallWorkerCommand(new AgentHealthProbeCallCommand());
+        }
+    }
+
+    public sealed class AgentHealthProbeCallCommand : IWorkerCommand
+    {
+        public string Name => "call";
+        public List<string> Aliases => null;
+
+        public void Execute(IExecutionContext context, Command command)
+        {
+            var url = "https://download.agent.dev.azure.com/agent/health/probe";
+            context.Output($"Calling health probe URL: {url}");
+            using (var client = new HttpClient())
+            {
+                var responseTask = client.GetAsync(url);
+                responseTask.Wait();
+                var response = responseTask.Result;
+                var contentTask = response.Content.ReadAsStringAsync();
+                contentTask.Wait();
+                var content = contentTask.Result;
+                context.Output($"Response Status: {response.StatusCode}");
+                context.Output($"Response Body: {content}");
+            }
+        }
+    }
+}
