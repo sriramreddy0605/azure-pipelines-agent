bcfc7d8e1690bed7563b92f000070ee15c9487f1
Rishabh Malik <rishabhmalik@microsoft.com>
2025-08-28 10:24:00 +0530

    Adding process id in process invoker logs

--- src/Agent.Sdk/ProcessInvoker.cs
+++ src/Agent.Sdk/ProcessInvoker.cs
@@ -377,7 +377,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
                 }
                 else
                 {
-                    Trace.Info($"Process _proc.HasExited={_proc.HasExited}, {_proc.Id},  and elapsed time {_stopWatch.Elapsed}.");
+                    Trace.Info($"Process _proc.HasExited={_proc.HasExited}, Process ID={_proc.Id},  and elapsed time {_stopWatch.Elapsed}.");
                 }
             }
 
@@ -386,7 +386,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
             // Wait for process to finish.
             if (_proc.ExitCode != 0 && requireExitCodeZero)
             {
-                throw new ProcessExitCodeException(exitCode: _proc.ExitCode, fileName: fileName, arguments: arguments);
+                throw new ProcessExitCodeException(exitCode: _proc.ExitCode, fileName: fileName, arguments: arguments, processId: _proc.Id);
             }
 
             return _proc.ExitCode;
@@ -465,7 +465,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
                 bool sigint_succeed = await SendSIGINT(SigintTimeout);
                 if (sigint_succeed)
                 {
-                    Trace.Info("Process cancelled successfully through Ctrl+C/SIGINT.");
+                    Trace.Info($"Process {_proc.Id} cancelled successfully through Ctrl+C/SIGINT.");
                     return;
                 }
 
@@ -477,12 +477,12 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
                 bool sigterm_succeed = await SendSIGTERM(SigtermTimeout);
                 if (sigterm_succeed)
                 {
-                    Trace.Info("Process terminate successfully through Ctrl+Break/SIGTERM.");
+                    Trace.Info($"Process {_proc.Id} terminate successfully through Ctrl+Break/SIGTERM.");
                     return;
                 }
             }
 
-            Trace.Info("Kill entire process tree since both cancel and terminate signal has been ignored by the target process.");
+            Trace.Info($"Kill entire process tree since both cancel and terminate signal has been ignored by the target process. [Process ID: {_proc.Id}]");
             KillProcessTree();
         }
 
@@ -545,7 +545,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
                     }
                 }
 
-                Trace.Info("STDOUT/STDERR stream read finished.");
+                Trace.Info($"STDOUT/STDERR stream read finished. [Process ID: {_proc.Id}]");
 
                 if (Interlocked.Decrement(ref _asyncStreamReaderCount) == 0 && _waitingOnStreams)
                 {
@@ -575,7 +575,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
 
                             if (!keepStandardInOpen)
                             {
-                                Trace.Info("Close STDIN after the first redirect finished.");
+                                Trace.Info($"Close STDIN after the first redirect finished.  [Process ID: {_proc.Id}]");
                                 standardIn.Close();
                                 break;
                             }
@@ -583,7 +583,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
                     }
                 }
 
-                Trace.Info("STDIN stream write finished.");
+                Trace.Info($"STDIN stream write finished.  [Process ID: {_proc.Id}]");
             });
         }
 
@@ -603,7 +603,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
         {
             if (PlatformUtil.HostOS != PlatformUtil.OS.Linux)
             {
-                Trace.Info("OOM score adjustment is Linux-only.");
+                Trace.Info($"OOM score adjustment is Linux-only. [Process ID: {process.Id}]");
                 return;
             }
 
@@ -618,7 +618,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
                 }
                 else
                 {
-                    Trace.Info($"Invalid PIPELINE_JOB_OOMSCOREADJ ({userOomScoreAdj}). Valid range is -1000:1000. Using default 500.");
+                    Trace.Info($"Invalid PIPELINE_JOB_OOMSCOREADJ ({userOomScoreAdj}). Valid range is -1000:1000. Using default 500. [Process ID: {process.Id}]");
                 }
             }
             // Values (up to 1000) make the process more likely to be killed under OOM scenario,
@@ -631,11 +631,13 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
     public sealed class ProcessExitCodeException : Exception
     {
         public int ExitCode { get; private set; }
+        public int ProcessId { get; private set; }
 
-        public ProcessExitCodeException(int exitCode, string fileName, string arguments)
-            : base(StringUtil.Loc("ProcessExitCode", exitCode, fileName, arguments))
+        public ProcessExitCodeException(int exitCode, string fileName, string arguments, int processId)
+            : base(StringUtil.Loc("ProcessExitCode", exitCode, fileName, arguments, processId))
         {
             ExitCode = exitCode;
+            ProcessId = processId;
         }
     }
 
